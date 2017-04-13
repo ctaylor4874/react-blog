7820d3763fbab6731628dd7cad3f85f457688bb9
Cody Taylor <ctaylor4874@gmail.com>
2017-04-13 15:10:09 -0500

    Create new post page now posts to backend

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,29 @@
 /**
  * Created by cody on 4/13/17.
  */
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
-export default () => {
-    return <div>List of Blog Posts</div>
-};
+class PostsIndex extends Component {
+    componentWillMount() {
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                List of blog posts
+            </div>
+        )
+    };
+}
+
+// The same as mapDispatchToProps
+export default connect(null, {fetchPosts})(PostsIndex);
