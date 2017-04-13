fde373b48a1508781bef4329339363ccf9d5110f
Cody Taylor <ctaylor4874@gmail.com>
2017-04-13 12:53:02 -0500

    Setup initial routes

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+/**
+ * Created by cody on 4/13/17.
+ */
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+
+import App from './components/app';
+
+export default(
+    <Route path="/" component={App}/>
+);
